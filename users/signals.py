fe88c402cdd7ee3ab0498ad32a2149ab41732f7e
Nezan Khan <nizankhan@gmail.com>
2022-10-15 02:15:40 -0700

    Fixed Login profile issue

--- users/signals.py
+++ users/signals.py
@@ -5,11 +5,11 @@ from .models import Profile #we will be creating a profile in function
 
 
 
-@receiver(post_save,sender= User) #when user is saved send this signal
+@receiver(post_save,sender=User) #when user is saved send this signal
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
 @receiver(post_save,sender=User)
 def save_profile(sender,instance,**kwargs):
-    instance.Profile.save()
+    instance.profile.save()
